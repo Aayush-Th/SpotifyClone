f835e6a486abd83c950541b830d35efdbfc698b5
Aayush-Th <thakuraayush633@gmail.com>
2025-05-17 13:41:30 +0530

    First Commit

---
+++ spotify.js
@@ -0,0 +1,42 @@
+console.log(`lets write javascript`);
+
+ async function getSongs(){
+    let a = await fetch(`/music/`)
+ let response = await a.text();
+  console.log(response)
+  let div = document.createElement("div")
+  div.innerHTML = response
+   let as = div.getElementsByTagName("a")
+  let songs = []
+   for(let index = 0; index < as.length; index++){
+    const element = as[index];
+    if(element.href.endsWith(".mp3")){
+        songs.push(element.href.split("/music/")[1])
+    }
+   }
+   return songs
+}
+
+async function main(){
+let songs = await getSongs()
+console.log(songs)
+
+let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+for(const song of songs){
+   songUl.innerHTML = songUl.innerHTML + `<li> ${song.replace("ytmp3free.cc"," ")} </li>`;
+}
+
+
+var audio = new Audio(songs[0])
+// audio.play()
+
+audio.addEventListener("loadeddata", () => {
+   console.log(audio.duration,audio.currentSrc,audio.currentTime)
+});
+
+}
+
+
+getSongs()
+
+main()
